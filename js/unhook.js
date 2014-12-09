e3f66aae67ae9ab7d54f5073a46eff6ca60c7be2
Troy Griggs <troy.griggs@gmail.com>
2014-12-09 17:32:59 -0500

    added scroll hook class

--- js/unhook.js
+++ js/unhook.js
@@ -54,20 +54,19 @@
 
 				// From top of page ... 
 				if ( $win.scrollTop() + paddingTop >= $this.offset().top ) {
-					//target.removeClass("uh-scroll-done uh-up-scroll").addClass("uh-down-scroll");
-					target.css({'position':'fixed','top': pixelPaddingTop, 'bottom':'auto', left: leftInset });
+					target.css({'position':'fixed','top': pixelPaddingTop, 'bottom':'auto', left: leftInset })
+						.addClass("down-scroll");;
 				} 
 				// ... to bottom of container ...
 				if ( $win.scrollTop() >= containerBottom - paddingTop - targetHeight ) {
 					console.log("scrollTop: " + $win.scrollTop())
 					console.log("Bottom: " + containerBottom);
-					//target.removeClass("uh-down-scroll uh-up-scroll").addClass("uh-scroll-done");
 					target.css({'position':'absolute', 'top':'auto', 'bottom': pixelPaddingBottom, left: 'auto' });
 				} 
 				// .. Back to top of page
-				else if ( $win.scrollTop() <= $this.offset().top ) {
-					//target.removeClass("uh-scroll-done uh-down-scroll").addClass("uh-up-scroll");
-					target.css({'position':'absolute', 'top': topOfPage, 'bottom':'auto', left: leftInset });
+				else if ( target.hasClass("down-scroll") && $win.scrollTop() <= $this.offset().top ) {
+					target.css({'position':'absolute', 'top': topOfPage, 'bottom':'auto', left: leftInset })
+						.removeClass("down-scroll");
 				}
 			});
 			
